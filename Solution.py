1400a537b6dfbb0d389a8b17a5778fa4045d2da3
olinto <olintoba@gmail.com>
2024-04-12 15:15:42 -0300

    partial Class Solution implementation

--- Solution.py
+++ Solution.py
@@ -7,9 +7,30 @@ class Solution:
      self.inst : Instance =  inst 
      self.fo = 0
      self.flowCap = inst.flowCap.copy()
-
+   
+     
+     self.collectedD ={}
+     self.uncollectedD ={}
+     self.flowD ={}
+     for d in range(self.inst.nNodes):
+        self.collectedD[d] = [] 
+        self.uncollectedD[d] = self.inst.Vd[d].copy() 
+        self.flowD[d] = [-1] * len(self.inst.Vd[d]) 
+     
     
    def printS(self):
 
       print("Objective Function: ",self.fo)    
       print("Flow available capacity: ",self.flowCap)
+
+      print("Collected Items:"); 
+      for d in range(self.inst.nNodes): 
+         print("D",d,": Collected",self.collectedD[d])
+
+      print("Uncollected Items:"); 
+      for d in range(self.inst.nNodes): 
+         print("D",d,": uncollected",self.uncollectedD[d])
+
+      print("Flow of collected items: ")
+      for d in range(self.inst.nNodes): 
+         print("D",d,self.flowD[d])
